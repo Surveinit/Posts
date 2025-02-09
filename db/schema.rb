9a32b3ac714a0c0d55922e33a9157268ce44b595
Surve <surve8729@gmail.com>
2025-02-09 20:03:44 +0530

    Add like_count to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_09_142517) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_143045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_09_142517) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "like_count", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
