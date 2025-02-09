0a57428f5e029c3076972b41aa563e384d5e5367
Surve <surve8729@gmail.com>
2025-02-09 22:12:57 +0530

    Add not follow self validation

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,6 +1,13 @@
 class Follower < ApplicationRecord
   validates :follower_id, uniqueness: { scope: :following_id, message: "is already following this user" }
+  validate :cannot_follow_self
 
   belongs_to :follower, class_name: "User"
   belongs_to :following, class_name: "User"
+
+  private
+
+  def cannot_follow_self
+    errors.add(:following, "cannot be the same as follower_id") if follower_id == following_id
+  end
 end
