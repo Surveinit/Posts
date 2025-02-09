e15991d94e18946e0b6e598d1eef2f4c4a8a4295
Surve <surve8729@gmail.com>
2025-02-09 22:00:55 +0530

    For better readability

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,5 +1,6 @@
 class Follower < ApplicationRecord
-  validates :follower_id, uniqueness: { scope: :following_id }
+  validates :follower_id, uniqueness: { scope: :following_id, message: "is already following this user" }
+
   belongs_to :follower, class_name: "User"
   belongs_to :following, class_name: "User"
 end
