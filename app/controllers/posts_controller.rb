fd461a336a6a78154241d0f7c9a4cb652b74865e
Surve <surve8729@gmail.com>
2025-02-10 15:29:43 +0530

    Display all posts & specific post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(commentable: @post, parent_id: nil)
   end
 end
