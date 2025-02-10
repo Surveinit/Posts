942983060d7a5ce81212d10b17cd7be02d9a3d73
Surve <surve8729@gmail.com>
2025-02-10 15:10:11 +0530

    Edit routes for dynamic posts#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "posts/index"
-  get "posts/show"
-  get "users/index"
-  get "users/show"
+  resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show]
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
