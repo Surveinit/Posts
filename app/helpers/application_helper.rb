3893c5470b9cb73021c0cbb5f7ffabaf4fb4b58a
Surve <surve8729@gmail.com>
2025-02-12 18:23:08 +0530

    Combine body tags in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def body_class
+    classes = []
+
+    # for devise:
+    classes << "login-page" if controller_name == "sessions" && action_name == "new"
+    classes << "signup-page" if controller_name == "registrations" && action_name == "new"
+
+    # for posts:
+    classes << "posts-index-page" if controller_name == "posts" && action_name == "index"
+    classes << "posts-show-page" if controller_name == "posts" && action_name == "show"
+
+    classes.join(" ")
+  end
 end
